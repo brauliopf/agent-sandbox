d92afecaaf40ba02e70a0542bd4d1a9d18c2e632
Braulio Fernandes <brauliopf+github@gmail.com>
2024-06-08 15:07:31 -0400

    use target webworker on webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
+    target: 'webworker',
     entry: './src/index.js',
     output: {
         filename: './bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
     },
-    mode: 'production'
+    mode: 'development'
 };
